609c88df9ee8e70adf08a2b34fb09ffc86c4fd58
Pawel Smuzny <pawelsmuzny@hotmail.com>
2018-07-28 20:20:03 +0200

    zmiany na branchu 2

--- Powtorka2/Pawel.cs
+++ Powtorka2/Pawel.cs
@@ -8,5 +8,16 @@ namespace Powtorka2
 {
     class Pawel
     {
+        public int dodawanie(int a)
+        {
+            var wynik = a;
+            wynik = wynik * 5;
+            wynik = wynik / 2;
+            return wynik;
+
+
+
+
+        }
     }
 }
